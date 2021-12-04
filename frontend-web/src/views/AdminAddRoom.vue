be29ba3c52699fd1d2034f46c880066c0b2d2afc
Zoyolo <1638621112@qq.com>
2021-12-04 16:39:19 +0800

    小小的更改

--- frontend-web/src/views/AdminAddRoom.vue
+++ frontend-web/src/views/AdminAddRoom.vue
@@ -15,11 +15,11 @@
         <el-form-item label="机房行数:" required>
           <div style="display: flex; flex-direction: row;">
             <el-form-item prop="roomRow">
-              <el-input v-model.number="roomForm.roomRow" placeholder="请输入行数">
+              <el-input v-model.number="roomForm.roomRow" placeholder="请输入行数" @change="checkRow()">
               </el-input>
             </el-form-item>
             <el-form-item prop="roomCol">
-              <el-input v-model.number="roomForm.roomCol" placeholder="请输入列数">
+              <el-input v-model.number="roomForm.roomCol" placeholder="请输入列数" @change="checkCol()">
               </el-input>
             </el-form-item>
           </div>
@@ -45,27 +45,37 @@
     </el-col>
   </el-row>
   <el-dialog
-      title="确认创建"
+      title="机位图预览"
       v-model="dialogAddVisible"
+      center
   >
-    prompt:这里显示一下创建的样子
-    <el-card class="box-card">
-      <el-row
-          :gutter="20"
-          v-for="itemRow of roomForm.roomRow"
-          :key="itemRow"
-          :span="5" size="mini"
-          :offset=itemRow*2
-      >
-        <el-card>
-          <el-button
-              type="primary"
-              v-for="item of roomForm.roomCol"
-          >
-          </el-button>
-        </el-card>
-      </el-row>
-    </el-card>
+    <el-row justify="center">
+      <el-card>
+        <el-row
+            :gutter="20"
+            v-for="itemRow of roomForm.roomRow"
+            :key="itemRow"
+            :span="5"
+            :offset=itemRow*1
+        >
+          <el-card>
+            <el-button
+                v-for="itemCol of roomForm.roomCol "
+                :key="itemCol"
+                type="primary"
+            >
+            </el-button>
+          </el-card>
+        </el-row>
+      </el-card>
+    </el-row>
+
+    <el-row justify="center">
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogAddVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogAddVisible = false">确 定</el-button>
+      </div>
+    </el-row>
   </el-dialog>
 
 </template>
@@ -114,35 +124,10 @@ export default {
         roomCol: '',
         roomStatus: '',
       },
-
-
-      dateOptions: [{
-        value: '1',
-        label: '第一大节',
-      },
-        {
-          value: '2',
-          label: '第二大节',
-        },
-        {
-          value: '3',
-          label: '第三大节',
-        },
-        {
-          value: '4',
-          label: '第四大节',
-        },
-        {
-          value: '5',
-          label: '第五大节',
-        },
-      ],
-      rooms: [],
-      fileList: [],
-      uploadFileList: [],
     }
   },
   methods: {
+
     onSubmit() {
       // this.$refs.upload.submit()
       console.log("submit success");
@@ -151,91 +136,21 @@ export default {
     resetForm(formName) {
       this.$refs[formName].resetFields();
     },
-    httpRequest(param) {
-      console.log(param.file)
-      let flag = 1
-      this.$refs['form'].validate((valid) => {
-        if (valid) {
-          console.log(this.form)
-        } else {
-          console.log('error submit!!')
-          flag = 0
-        }
-      })
-      if (flag == 0) return
-      let fileObj = param.file // 相当于input里取得的files
-      let fd = new FormData() // FormData 对象
-      fd.append('file', fileObj) // 文件对象
-      fd.append('coursename', this.form.coursename)
-      fd.append('coursecontent', this.form.coursecontent)
-      fd.append('coursetime', this.form.coursetime)
-      fd.append('coursedate', this.form.coursedate)
-      fd.append('estimatedNumber', this.form.estimatedNumber)
-      fd.append('roomid', this.form.roomid)
-      fd.append('isshared', this.form.isshared)
-
-      let config = {
-        headers: {
-          'Content-Type': 'multipart/form-data'
-        }
-      }
-      request.post('http://rap2api.taobao.org/app/mock/294824/getMessageList', fd, config).then(res => {
-        if (res.code === '0') {
-          this.$message({
-            type: "success",
-            message: "提交成功"
-          })
-          this.fileList = []
-        } else {
-          this.$message({
-            type: "error",
-            message: res.msg
-          })
-        }
-      })
-    },
-
-    handleRemove(file, fileList) {
-      this.fileList = fileList
-    },
-    handlePreview(file) {
-      console.log(file)
-    },
-    handleChange(file, fileList) {
-      this.fileList = fileList
-    },
-    exploreRooms() {
-      if (this.form.coursedate != '' && this.form.coursetime != '') {
-        //接口:需要返回规定日期，规定大节的所有机房信息
-        console.log(this.form)
-        this.rooms = [{
-          roomid: '1001',
-          number: '20'
-        }, {
-          roomid: '1002',
-          number: '20'
-        }, {
-          roomid: '1003',
-          number: '20'
-        }]
-      }
-    },
-    checkNum() {
+    checkRow() {
       var regPos = /^[0-9]+.?[0-9]*/;
-      if (this.form.estimatedNumber <= 0 || this.form.estimatedNumber > 150 || !regPos.test(this.form
-          .estimatedNumber)) {
-        this.$message.error("请输入正确的数字!")
-        delete this.form.estimatedNumber;
+      if (this.roomForm.roomRow <= 0 || !regPos.test(this.roomForm.roomRow)) {
+        this.$message.error("请输入正确的行数!")
+        delete this.roomForm.roomRow;
       }
     },
-    handleSelectionChange(val) {
-      this.form.roomid = []
-      for (var j = 0; j < val.length; j++) {
-        console.log(val[j].roomid)
-        this.form.roomid.push(val[j].roomid)
+    checkCol() {
+      var regPos = /^[0-9]+.?[0-9]*/;
+      if (this.roomForm.roomCol <= 0 || !regPos.test(this.roomForm.roomCol)) {
+        this.$message.error("请输入正确的列数!")
+        delete this.roomForm.roomCol;
       }
-      console.log(this.form)
     }
+
   },
 }
 </script>
